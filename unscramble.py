a103341b8e9f431dadba36f09945c02441611d2d
Fakkinator <legitbob4@yahoo.com>
2020-04-07 20:19:53 -0400

    refactor adj names

--- unscramble.py
+++ unscramble.py
@@ -8,10 +8,10 @@ from PIL import Image
 class Node(object):
     def __init__(self):
         self.img = None
-        self.up = None
-        self.down = None
-        self.left = None
-        self.right = None
+        self.u = None
+        self.d = None
+        self.l = None
+        self.r = None
 
 class ImGrid(object):
     def __init__(self, dims):
@@ -19,46 +19,45 @@ class ImGrid(object):
         self.xmax = self.x_dim - 1
         self.ymax = self.y_dim - 1
         self.nodes = [Node() for _ in range(self.x_dim * self.y_dim)]
-        # self.nodes = []
-        # for _ in range(self.x_dim * self.y_dim):
-        #     self.nodes.append(Node())
         with open('nodes.json', 'r') as f:
             cons = json.loads(f.read())
 
         for i, vals in enumerate(cons):
             node = self.nodes[i]
             u, d, l, r = vals
-            if u is not None: node.up = self.nodes[u]
-            if d is not None: node.down = self.nodes[d]
-            if l is not None: node.left = self.nodes[l]
-            if r is not None: node.right = self.nodes[r]
+            if u is not None: node.u = self.nodes[u]
+            if d is not None: node.d = self.nodes[d]
+            if l is not None: node.l = self.nodes[l]
+            if r is not None: node.r = self.nodes[r]
 
     def pack(self, slices):
         for i, img in enumerate(slices):
             self.nodes[i].img = img
 
+    @staticmethod
+    def get_slices(path):
+        im = Image.open(path)
+        slices = []
+        I = 128
+        J = 128
+        for j in range(0, 1920, J):
+            for i in range(0, 1408, I):
+                print(i, j)
+                box = (i, j, i+I, j+J)
+                # box = (j, i, j+J, i+I)
+                slices.append(im.crop(box))
+
+        return slices
 
 def unscramble(img):
     pass
 
-
 def test():
-    im = Image.open('1.png')
-    slices = []
-    I = 128
-    J = 128
-    for j in range(0, 1920, J):
-        for i in range(0, 1408, I):
-            print(i, j)
-            box = (i, j, i+I, j+J)
-            # box = (j, i, j+J, i+I)
-            slices.append(im.crop(box))
 
     grid = ImGrid((11, 15))
+    slices = grid.get_slices('1.png')
     grid.pack(slices)
-    grid.nodes[30].img.show()
-    grid.nodes[30].up.img.show()
-    grid.nodes[30].left.img.show()
+    grid.nodes[2].img.show()
 
 
 
